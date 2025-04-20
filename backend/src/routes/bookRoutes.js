2158d0b7f6c35aea0190a614df3cb476f0aa852f
ola <hollabodey.kilaso@gmail.com>
2025-04-20 03:58:23 +0100

    Enhance book routes with pagination and user authorization; integrate protectRoute middleware for secure access

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cloudinary from "../lib/cloudinary.js"
 import Book from "../models/Book.js"
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -24,7 +25,7 @@ router.post("/", protectRoute, async (req, res) => {
             caption,
             rating,
             image: imageUrl,
-            // user: req.user._id,
+            user: req.user._id,
         })
 
         await newBook.save();
@@ -33,6 +34,68 @@ router.post("/", protectRoute, async (req, res) => {
     } catch (error) {
         console.log("Error creating book", error);
         res.status(500).json({ message: error.message })
+    }
+})
+
+
+// pagination = => infinite loading
+router.get("/", protectRoute, async (req, res) => {
+    // example call from react native - frontend
+    // const response = await fetch("http://localhost:3000/api/books?page=1&limit=5")
+
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+        const books = await Book.find()
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate("user", "usernmae profileImage")
+
+        const totalBooks = await Book.countDocuments();
+
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPage: Math.ceil(totalBooks / limit),
+        });
+
+    } catch (error) {
+        console.log("Error in get all books route", error);
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.delete("/:id", protectRoute, async (req,res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) return res.status(400).json({message: "Book not found"});
+
+        // check if user is the creator of the book
+        if(book.user.toString() !== req.user._id.toString())
+            return res.status(401).json({message: "Unauthorized"});
+
+        // delete image from cloudinary
+        if(book.image && book.image.includes("cloudinary")){
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(publicId);
+                
+            } catch (deleteError) {
+                console.log("Error deleting image from cloudinary", deleteError)
+            }
+        }
+
+        await book.deleteOne();
+
+        res.json({message: "Book deleted successfully"});
+    } catch (error) {
+        console.log("Error deleting book", error);
+        res.status(500).json({message: "Internal server error"});
+
     }
 })
 
