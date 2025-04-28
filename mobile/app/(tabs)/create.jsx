9e231813a354fcebbf67d37689f2c4adf03b859f
ola <hollabodey.kilaso@gmail.com>
2025-04-28 13:54:35 +0100

    feat: implement image picker functionality in Create screen; add caption input and loading state

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,62 +1,124 @@
-import { View, Text, KeyboardAvoidingView, Platform, ScrollView, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { useRouter } from 'expo-router'
-import styles from "../../assets/styles/create.styles"
-import { Ionicons } from '@expo/vector-icons'
-import COLORS from "../../constant/colors"
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constant/colors";
+import * as ImagePicker from "expo-image-picker"
+import * as FileSystem from "expo-file-system"
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const [title, setTitle] = useState("")
-  const [caption, setCaption] = useState("")
-  const [rating, setRating] = useState(3)
-  const [image, setImage] = useState(null)
-  const [imageBase64, setImageBase64] = useState(null)
-  const [loading, setLoading] = useState(false)
-  
+  const router = useRouter();
 
-  const router = useRouter()
-  const pickImage = async () => {}
-  const handleSubmit = async () => {}
+  const pickImage = async () => {
+    try {
+      // request permission if needed
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+          if (status !== "granted") {
+          Alert.alert(
+            "Permission Denied",
+            "We need camera roll persmission to upload an image"
+          );
+          return;
+        }
+      }
+
+      // Launch image library
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        allowsEditing:true,
+        aspect:[4,3],
+        quality:0.5, // Lower image quality for smaller base64
+        base64:true
+      })
+
+      if (!result.canceled){
+        setImage(result.assets[0].uri)
+        
+        // If Base64 is provided, use it
+        if(result.assets[0].base64){
+          setImageBase64(result.assets[0].base64);
+        } else{
+          const base64 = await FileSystem.readAsStringAsync(result.assets[0].uri, {
+            encoding: FileSystem.EncodingType.Base64,  
+          })
+          setImageBase64(base64);
+        }
+
+      }
+    } catch (error) {
+      console.log("Error picking image:", error);
+      Alert.alert("Error", "There was a problem selecting your image");
+    }
+  };
+
+  const handleSubmit = async () => {};
 
   const renderRatingPicker = () => {
     const stars = [];
-    for (let i = 1 ;i <= 5; i++){
+    for (let i = 1; i <= 5; i++) {
       stars.push(
-        <TouchableOpacity key ={i} onPress={() => setRating(i)} style={styles.starButton}>
-          <Ionicons 
+        <TouchableOpacity
+          key={i}
+          onPress={() => setRating(i)}
+          style={styles.starButton}
+        >
+          <Ionicons
             name={i <= rating ? "star" : "star-outline"}
             size={32}
             color={i <= rating ? "#f4b400" : COLORS.textSecondary}
           />
         </TouchableOpacity>
-      )
+      );
     }
-    return <View style={styles.ratingContainer}>{stars}</View>
-  }
-  
+    return <View style={styles.ratingContainer}>{stars}</View>;
+  };
+
   return (
     <KeyboardAvoidingView
-      style ={{ flex: 1 }}
+      style={{ flex: 1 }}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <ScrollView
-        contentContainerStyle ={styles.container}
+        contentContainerStyle={styles.container}
         style={styles.scrollViewStyle}
       >
-        <View
-          style = {styles.card}
-        >
+        <View style={styles.card}>
           {/* Header */}
-          <View style ={styles.header}>
+          <View style={styles.header}>
             <Text style={styles.title}>Add Book Recommendation</Text>
-            <Text style={styles.subtitle}>Share your favourite reads with others</Text>
+            <Text style={styles.subtitle}>
+              Share your favourite reads with others
+            </Text>
           </View>
 
           <View styles={styles.form}>
             {/* BOOK TITLE */}
             <View style={styles.formGroup}>
-              <Text style= {styles.label}>Book Title</Text>
+              <Text style={styles.label}>Book Title</Text>
               <View style={styles.inputContainer}>
                 <Ionicons
                   name="book-outline"
@@ -66,10 +128,9 @@ export default function Create() {
                 />
                 <TextInput
                   style={styles.input}
-                  placeholder='Enter book title'
+                  placeholder="Enter book title"
                   placeholderTextColor={COLORS.placeholderText}
                 />
-
               </View>
             </View>
 
@@ -78,10 +139,63 @@ export default function Create() {
               <Text style={styles.label}>Your Rating</Text>
               {renderRatingPicker()}
             </View>
+
+            {/* IMAGE */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Image</Text>
+              <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
+                {image ? (
+                  <Image source={{ uri: image }} style={styles.previewImage} />
+                ) : (
+                  <View style={styles.placeholderContainer}>
+                    <Ionicons
+                      name="image-outline"
+                      size={40}
+                      color={COLORS.textSecondary}
+                    />
+                    <Text style={styles.placeholderText}>
+                      Tap to select image
+                    </Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+            </View>
+
+            {/* CAPTION */}
+            <View>
+              <Text style={styles.formGroup}>Caption</Text>
+              <TextInput 
+                style={styles.textArea}
+                placeholder="Write your review or thoughts about this book..."
+                placeholderTextColor={COLORS.placeholderText}
+                value={caption}
+                onChangeText={setCaption}
+                multiline
+              />
+
+            </View>
+
+            {/* BUTTON */}
+            <TouchableOpacity style={styles.button} onPress={handleSubmit} disabled={loading}>
+              {loading ? (
+                <ActivityIndicator color={COLORS.white} />
+
+              ) : (
+                <>
+                  <Ionicons 
+                  name="cloud-upload-outline"
+                  size={20}
+                  color={COLORS.white}
+                  style={styles.buttonIcon}
+                  />
+                  
+                  <Text style={styles.buttonText}>Share</Text>
+                </>
+              )}
+            </TouchableOpacity>
           </View>
         </View>
-        
       </ScrollView>
     </KeyboardAvoidingView>
-  )
+  );
 }
