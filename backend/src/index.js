feb9e1bbaa016ff9048cf67820da22b6b79ef4ec
ola <hollabodey.kilaso@gmail.com>
2025-05-02 21:41:18 +0100

    refactor: comment out cors middleware in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
