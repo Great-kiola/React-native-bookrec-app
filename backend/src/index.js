be977bdaac6fcdb275f724e6783efadbe101b8e7
ola <hollabodey.kilaso@gmail.com>
2025-05-02 21:39:05 +0100

    refactor: comment out dotenv require statements and adjust cloudinary configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+// require('dotenv').config();
 
 
 // routes
@@ -11,8 +12,11 @@ import bookRoutes from "./routes/bookRoutes.js";
 const app = express(); 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
