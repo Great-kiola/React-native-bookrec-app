7a332326b2150c3b3e7d674019ae9f2af9e4dfec
ola <hollabodey.kilaso@gmail.com>
2025-05-05 18:50:55 +0100

    feat: implement user books fetching in Profile component

--- mobile/app/(tabs)/profile.jsx
+++ mobile/app/(tabs)/profile.jsx
@@ -1,7 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import {useRouter} from "expo-router";
 
 export default function Profile() {
+  const[books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const router = useRouter();
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true)
+      const response = fetch(`${API_URL}/books/user`, {
+        headers : { Authorization : `Bearer ${token}`},
+      });
+
+      const data = (await response).json();
+      if(!response.ok) throw new Error(data.message || "Failed to fetch user books");
+      setBooks(data);
+
+    } catch (error) {
+      console.error("Erorr fetching data:", error);
+      Alert.alert("Error", "Failed to load profile data. Pull down to refresh.");
+    } finally{
+      setIsLoading(false);
+    }
+  };
+
+
+
   return (
     <View>
       <Text>Profile Tab</Text>
