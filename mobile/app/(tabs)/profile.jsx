ea3b96613aeab3b8c4fa77c635ef8fc5a9d280a2
ola <hollabodey.kilaso@gmail.com>
2025-05-06 20:06:22 +0100

    feat: implement Profile component with user data fetching and add ProfileHeader and LogoutButton components

--- mobile/app/(tabs)/profile.jsx
+++ mobile/app/(tabs)/profile.jsx
@@ -1,38 +1,52 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import {useRouter} from "expo-router";
+import { View, Text, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import { API_URL } from "../../constant/api";
+import { useAuthStore } from "../../store/authStore";
+import styles from "../../assets/styles/profile.styles"
+import ProfileHeader from "../../components/ProfileHeader";
+import LogoutButton from "../../components/LogoutButton";
 
 export default function Profile() {
-  const[books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+
+  const token = useAuthStore();
 
   const router = useRouter();
 
   const fetchData = async () => {
     try {
-      setIsLoading(true)
+      setIsLoading(true);
       const response = fetch(`${API_URL}/books/user`, {
-        headers : { Authorization : `Bearer ${token}`},
+        headers: { Authorization: `Bearer ${token}` },
       });
 
       const data = (await response).json();
-      if(!response.ok) throw new Error(data.message || "Failed to fetch user books");
+      if (!response.ok)
+        throw new Error(data.message || "Failed to fetch user books");
       setBooks(data);
-
     } catch (error) {
       console.error("Erorr fetching data:", error);
-      Alert.alert("Error", "Failed to load profile data. Pull down to refresh.");
-    } finally{
+      Alert.alert(
+        "Error",
+        "Failed to load profile data. Pull down to refresh."
+      );
+    } finally {
       setIsLoading(false);
     }
   };
 
-
+  useEffect(() => {
+    fetchData()
+  }, []);
 
   return (
-    <View>
-      <Text>Profile Tab</Text>
+    <View style= {styles.container}>
+      <ProfileHeader/>
+      <LogoutButton/>
+      
     </View>
-  )
+  );
 }
